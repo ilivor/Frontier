f7d2ec1a9c6fd786d6c389ffa407f611f889a4f6
Zabka v puairike <166412826+ilivor@users.noreply.github.com>
2026-01-10 08:30:49 +0700

    Add HermeticCodePaperComponent to HermeticSealDoor

--- Content.Shared/__/HermeticSealDoorComponent.cs
+++ Content.Shared/__/HermeticSealDoorComponent.cs
@@ -1,3 +1,12 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+using Content.Shared.Doors.Components;
+using Content.Shared.Prying.Components;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared.Doors.Components;
+
 [RegisterComponent, NetworkedComponent]
 public sealed partial class HermeticCodePaperComponent : Component
 {
